156be0979bad58e6338d4ccbbe390c6843674506
Oscar25Obed <oscar251626@gmail.com>
2024-11-07 01:48:06 -0500

    correciones

--- Flota-Front/src/app/app.component.spec.ts
+++ Flota-Front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Flota-front' title`, () => {
+  it(`should have the 'Flota-Front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Flota-front');
+    expect(app.title).toEqual('Flota-Front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Flota-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Flota-Front');
   });
 });
