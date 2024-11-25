9e139b3bc2b41c3f46a8906f2daf3ce00b3a7f08
Jose Camarena <jlcamarena2000@gmail.com>
2024-11-24 22:20:02 -0500

    Cambio en Rental para permitir la eliminación en cascada de Driver

--- backend/spring-boot-jwt-authentication/src/main/java/com/irojas/demojwt/Rental/Rental.java
+++ backend/spring-boot-jwt-authentication/src/main/java/com/irojas/demojwt/Rental/Rental.java
@@ -11,6 +11,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Data
 @Builder
@@ -29,6 +31,7 @@ public class Rental {
 
     @ManyToOne
     @JoinColumn(name = "driver_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Driver driver;
 
     @Column(nullable = false)
